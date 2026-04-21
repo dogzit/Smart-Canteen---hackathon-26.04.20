11ff7dcd3e9854f4032fed16cf888924e2e1eccd
Zoloo <bolorzoloko@gmail.com>
2026-04-21 09:10:39 +0800

    1

--- middleware.ts
+++ middleware.ts
@@ -6,18 +6,18 @@ export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   // Нэвтэрсэн хэрэглэгчийг Signup руу оруулахгүй
-  if (token && (pathname === "/signup" || pathname === "/")) {
+  if (token && (pathname === "/auth" || pathname === "/")) {
     return NextResponse.redirect(new URL("/smart-canteen", request.url));
   }
 
   // Нэвтрээгүй бол Smart Canteen руу оруулахгүй
   if (!token && pathname.startsWith("/smart-canteen")) {
-    return NextResponse.redirect(new URL("/signup", request.url));
+    return NextResponse.redirect(new URL("/auth", request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/", "/signup", "/smart-canteen/:path*"],
+  matcher: ["/", "/auth", "/smart-canteen/:path*"],
 };
